095fdebd2b9e3718a1660e379aa9db83adbdbc06
sonyagennova <48459577+sonyagennova@users.noreply.github.com>
2021-03-31 22:49:36 +0300

    ESlint search.js fixes

--- tcms/testruns/static/testruns/js/search.js
+++ tcms/testruns/static/testruns/js/search.js
@@ -1,167 +1,116 @@
-function pre_process_data(data, callback) {
-    const runIds = [],
-          planIds = []
-    data.forEach(function(element) {
-        runIds.push(element.id)
-        planIds.push(element.plan)
-    })
+function pre_process_data (data) {
+  const product_cache = {}
+  const tags_cache = {}
 
-    // get tags for all objects
-    const tagsPerRun = {}
-    jsonRPC('Tag.filter', {run__in: runIds}, function(tags) {
-        tags.forEach(function(element) {
-            if (tagsPerRun[element.run] === undefined) {
-                tagsPerRun[element.run] = []
-            }
+  data.forEach(function (element) {
+    // collect info about products
+    addResourceToData(element, 'tag', 'Tag.filter', tags_cache)
 
-            // push only if unique
-            if (tagsPerRun[element.run].indexOf(element.name) === -1) {
-                tagsPerRun[element.run].push(element.name)
-            }
-        })
-
-        jsonRPC('Product.filter', {plan__in: planIds}, function(products) {
-            products = arrayToDict(products)
-
-            // augment data set with additional info
-            data.forEach(function(element) {
-                if (element.id in tagsPerRun) {
-                    element.tag = tagsPerRun[element.id]
-                } else {
-                    element.tag = []
-                }
-
-                element.product_name = products[element.plan__product].name
-            });
-
-            callback({data: data}) // renders everything
-        })
-    })
+    if (element.plan_id in product_cache) {
+      element.product = product_cache[element.plan_id]
+    } else {
+      jsonRPC('TestPlan.filter', { pk: element.plan_id }, function (data) {
+        element.product = data[0].product
+      }, true)
+      product_cache[element.plan_id] = element.product
+    }
+  })
 }
 
+$(document).ready(function () {
+  const table = $('#resultsTable').DataTable({
+    pageLength: $('#navbar').data('defaultpagesize'),
+    ajax: function (data, callback, settings) {
+      const params = {}
 
-$(document).ready(function() {
-    var table = $("#resultsTable").DataTable({
-        pageLength: $('#navbar').data('defaultpagesize'),
-        ajax: function(data, callback, settings) {
-            var params = {};
+      if ($('#id_summary').val()) {
+        params.summary__icontains = $('#id_summary').val()
+      }
 
-            if ($('#id_summary').val()) {
-                params['summary__icontains'] = $('#id_summary').val();
-            }
+      if ($('#id_plan').val()) {
+        params.plan = $('#id_plan').val()
+      }
 
-            if ($('#id_after_start_date').val()) {
-                params['start_date__gte'] = $('#id_after_start_date').data('DateTimePicker').date().format('YYYY-MM-DD 00:00:00');
-            }
+      if ($('#id_product').val()) {
+        params.plan__product = $('#id_product').val()
+      };
 
-            if ($('#id_before_start_date').val()) {
-                params['start_date__lte'] = $('#id_before_start_date').data('DateTimePicker').date().format('YYYY-MM-DD 23:59:59');
-            }
+      if ($('#id_version').val()) {
+        params.product_version = $('#id_version').val()
+      };
 
-            if ($('#id_after_stop_date').val()) {
-                params['stop_date__gte'] = $('#id_after_stop_date').data('DateTimePicker').date().format('YYYY-MM-DD 00:00:00');
-            }
+      if ($('#id_build').val()) {
+        params.build = $('#id_build').val()
+      };
 
-            if ($('#id_before_stop_date').val()) {
-                params['stop_date__lte'] = $('#id_before_stop_date').data('DateTimePicker').date().format('YYYY-MM-DD 23:59:59');
-            }
+      if ($('#id_manager').val()) {
+        params.manager__username__startswith = $('#id_manager').val()
+      };
 
-            if ($('#id_after_planned_start').val()) {
-                params['planned_start__gte'] = $('#id_after_planned_start').data('DateTimePicker').date().format('YYYY-MM-DD 00:00:00');
-            }
+      if ($('#id_default_tester').val()) {
+        params.default_tester__username__startswith = $('#id_default_tester').val()
+      };
 
-            if ($('#id_before_planned_start').val()) {
-                params['planned_start__lte'] = $('#id_before_planned_start').data('DateTimePicker').date().format('YYYY-MM-DD 23:59:59');
-            }
+      updateParamsToSearchTags('#id_tag', params)
 
-            if ($('#id_after_planned_stop').val()) {
-                params['planned_stop__gte'] = $('#id_after_planned_stop').data('DateTimePicker').date().format('YYYY-MM-DD 00:00:00');
-            }
+      params.stop_date__isnull = $('#id_running').is(':checked')
 
-            if ($('#id_before_planned_stop').val()) {
-                params['planned_stop__lte'] = $('#id_before_planned_stop').data('DateTimePicker').date().format('YYYY-MM-DD 23:59:59');
-            }
+      dataTableJsonRPC('TestRun.filter', params, callback, pre_process_data)
+    },
+    columns: [
+      { data: 'id' },
+      {
+        data: null,
+        render: function (data, type, full, meta) {
+          result = '<a href="/runs/' + data.id + '/" target="_parent">' + escapeHTML(data.summary) + '</a>'
+          if (data.stop_date) {
+            result += '<p class="help-block">' + data.stop_date + '</p>'
+          }
+          return result
+        }
+      },
+      {
+        data: null,
+        render: function (data, type, full, meta) {
+          return '<a href="/plan/' + data.plan_id + '/" target="_parent">TP-' + data.plan_id + ': ' + escapeHTML(data.plan) + '</a>'
+        }
+      },
+      { data: 'product' },
+      { data: 'product_version' },
+      { data: 'build' },
 
-            if ($('#id_plan').val()) {
-                params['plan'] = $('#id_plan').val();
-            }
+      { data: 'manager' },
+      { data: 'default_tester' },
+      {
+        data: 'tag',
+        render: renderFromCache
+      }
+    ],
+    dom: 't',
+    language: {
+      loadingRecords: '<div class="spinner spinner-lg"></div>',
+      processing: '<div class="spinner spinner-lg"></div>',
+      zeroRecords: 'No records found'
+    },
+    order: [[0, 'asc']]
+  })
 
-            if ($('#id_product').val()) {
-                params['plan__product'] = $('#id_product').val();
-            };
+  hookIntoPagination('#resultsTable', table)
 
-            if ($('#id_version').val()) {
-                params['plan__product_version'] = $('#id_version').val();
-            };
+  $('#btn_search').click(function () {
+    table.ajax.reload()
+    return false // so we don't actually send the form
+  })
 
-            if ($('#id_build').val()) {
-                params['build'] = $('#id_build').val();
-            };
+  $('#id_product').change(function () {
+    update_version_select_from_product()
+  })
 
-            if ($('#id_manager').val()) {
-                params['manager__username__startswith'] = $('#id_manager').val();
-            };
+  $('#id_version').change(function () {
+    update_build_select_from_version(true)
+  })
 
-            if ($('#id_default_tester').val()) {
-                params['default_tester__username__startswith'] = $('#id_default_tester').val();
-            };
+  $('.bootstrap-switch').bootstrapSwitch()
 
-            updateParamsToSearchTags('#id_tag', params);
-
-            params['stop_date__isnull'] = $('#id_running').is(':checked');
-
-            dataTableJsonRPC('TestRun.filter', params, callback, pre_process_data);
-        },
-        columns: [
-            { data: "id" },
-            {
-                data: null,
-                render: function (data, type, full, meta) {
-                    result = '<a href="/runs/'+ data.id + '/">' + escapeHTML(data.summary) + '</a>';
-                    if (data.stop_date) {
-                        result += '<p class="help-block">' + data.stop_date + '</p>';
-                    }
-                    return result;
-                }
-            },
-            {
-                data: null,
-                render: function (data, type, full, meta) {
-                    return '<a href="/plan/'+ data.plan + '/">TP-' + data.plan + ': ' + escapeHTML(data.plan__name) + '</a>';
-                }
-            },
-            { data: "product_name" },
-            { data: "plan__product_version__value"},
-            { data: "build__name"},
-            { data: "manager__username" },
-            { data: "default_tester__username" },
-            { data: "tag" },
-        ],
-        dom: "t",
-        language: {
-            loadingRecords: '<div class="spinner spinner-lg"></div>',
-            processing: '<div class="spinner spinner-lg"></div>',
-            zeroRecords: "No records found"
-        },
-        order: [[ 0, 'asc' ]],
-    });
-
-    hookIntoPagination('#resultsTable', table);
-
-    $('#btn_search').click(function() {
-        table.ajax.reload();
-        return false; // so we don't actually send the form
-    });
-
-    $('#id_product').change(function() {
-        update_version_select_from_product();
-    });
-
-    $('#id_version').change(function() {
-        update_build_select_from_version(true);
-    });
-
-    $('.bootstrap-switch').bootstrapSwitch();
-
-    $('.selectpicker').selectpicker();
-});
+  $('.selectpicker').selectpicker()
+})
